3d582a1ff18971d564dd6b6d31a06e307c712679
TZFC <tianzifangchen@gmail.com>
2025-07-31 13:36:58 -0400

    break on STOP

--- app/vrc_osc_send.py
+++ app/vrc_osc_send.py
@@ -31,6 +31,7 @@ async def send_vrchat_osc(osc_queue: asyncio.Queue):
                 send_chat(message, immediate)
             elif request_type == "STOP":
                 close()
+                break
             else:
                 logger.warning("未知请求类型 %s", request_type)
             logger.info("请求 %s 成功, 还剩 %d 条请求", str(request),osc_queue.qsize())
