a49be103d581e3df712b828d14eec56c086d8105
TZFC <tianzifangchen@gmail.com>
2025-08-14 14:50:47 -0400

    fix not awaiting dispatch

--- app/bili_event_dispatch.py
+++ app/bili_event_dispatch.py
@@ -33,18 +33,18 @@ live_danmaku: LiveDanmaku = LiveDanmaku(
 
 
 # 启动事件分发
-def dispatch(event_name: str, event: dict, handler):
+async def dispatch(event_name: str, event: dict, handler):
     if CONFIG['events'][event_name] == 0:
         return
     elif CONFIG['events'][event_name] == 1:
-        handler(event, update_chatbox=True,
-                update_osc_param=False, osc_queue=osc_queue)
+        await handler(event, update_chatbox=True,
+                      update_osc_param=False, osc_queue=osc_queue)
     elif CONFIG['events'][event_name] == 2:
-        handler(event, update_chatbox=False,
-                update_osc_param=True, osc_queue=osc_queue)
+        await handler(event, update_chatbox=False,
+                      update_osc_param=True, osc_queue=osc_queue)
     elif CONFIG['events'][event_name] == 3:
-        handler(event, update_chatbox=True,
-                update_osc_param=True, osc_queue=osc_queue)
+        await handler(event, update_chatbox=True,
+                      update_osc_param=True, osc_queue=osc_queue)
     else:
         logger.warning(
             f"未知{event_name}用户设置{CONFIG['events'][event_name]}")
@@ -55,7 +55,7 @@ def dispatch(event_name: str, event: dict, handler):
 
 @live_danmaku.on('INTERACT_WORD_V2')
 async def on_interact(event: dict):
-    dispatch('enter', event, handle_enter)
+    await dispatch('enter', event, handle_enter)
 
 # 收到弹幕或表情包
 
@@ -64,9 +64,9 @@ async def on_interact(event: dict):
 async def on_danmaku(event: dict):
     message_type: int = event["data"]["info"][0][MSG_TYPE_IDX]
     if message_type == TEXT_TYPE:  # 文字弹幕
-        dispatch('danmaku', event, handle_text)
+        await dispatch('danmaku', event, handle_text)
     elif message_type == EMOTICON_TYPE:  # 表情包
-        dispatch('emoticon', event, handle_emoticon)
+        await dispatch('emoticon', event, handle_emoticon)
     else:
         logger.warning("未知弹幕类型 %d", message_type)
 
@@ -75,25 +75,25 @@ async def on_danmaku(event: dict):
 
 @live_danmaku.on('SEND_GIFT')
 async def on_gift(event: dict):
-    dispatch('gift', event, handle_gift)
+    await dispatch('gift', event, handle_gift)
 
 # 收到sc
 
 
 @live_danmaku.on('SUPER_CHAT_MESSAGE')
 async def on_sc(event: dict):
-    dispatch('sc', event, handle_sc)
+    await dispatch('sc', event, handle_sc)
 
 # 收到舰长
 
 
 @live_danmaku.on('GUARD_BUY')
 async def on_guard_buy(event: dict):
-    dispatch('guard', event, handle_guard)
+    await dispatch('guard', event, handle_guard)
 
 # 收到警告
 
 
 @live_danmaku.on('WARNING')
 async def on_warning(event: dict):
-    dispatch('warning', event, handle_warning)
+    await dispatch('warning', event, handle_warning)
