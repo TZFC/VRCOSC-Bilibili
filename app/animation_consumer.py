211457f79de1fefaea3b0f4de9de4857a6c34cea
TZFC <tianzifangchen@gmail.com>
2025-08-16 13:56:38 -0400

    Animation tests passed. TODO:  test general event_id and event_num

--- app/animation_consumer.py
+++ app/animation_consumer.py
@@ -31,18 +31,19 @@ async def animation_loop():
             current_time = 0
         for index, (animation_name, pending_value) in enumerate(animation_counts.items()):
             animation_time: int = CONFIG["animation_accumulate"]["animation_time"][index]
+            animated_parameter: str = CONFIG["animation_accumulate"]["animated_parameter"][index]
             if current_time % animation_time != 0:
                 # 还没到汇报时间
                 continue
             if pending_value >= MAX_COUNT_PER_SECOND:
                 # 堆积超过上限，汇报上限
-                update_parameter(animation_name, int2f8(MAX_COUNT_PER_SECOND))
+                update_parameter(animated_parameter, int2f8(MAX_COUNT_PER_SECOND))
                 animation_counts[animation_name] -= MAX_COUNT_PER_SECOND
             elif pending_value > 0:
                 # 堆积不足上限，汇报所有堆积
-                update_parameter(animation_name, int2f8(pending_value))
+                update_parameter(animated_parameter, int2f8(pending_value))
                 animation_counts[animation_name] = 0
             else:
                 # 无堆积，归零
-                update_parameter(animation_name, 0)
+                update_parameter(animated_parameter, 0)
                 animation_counts[animation_name] = 0
