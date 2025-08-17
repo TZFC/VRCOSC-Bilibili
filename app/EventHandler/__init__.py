e61f5537a71bb202bce7dea282d79a88d0950c97
TZFC <tianzifangchen@gmail.com>
2025-08-17 02:52:34 -0400

    add init files for pylint

---
+++ app/EventHandler/__init__.py
@@ -0,0 +1,9 @@
+__all__ = [
+    "danmaku_emoticon_handler",
+    "danmaku_text_handler",
+    "enter_handler",
+    "gift_handler",
+    "guard_handler",
+    "warning_handler",
+    "sc_handler",
+]
