341d669d3e3e6b4c664841e8343e53d3e76c5f7f
TZFC <tianzifangchen@gmail.com>
2025-08-17 03:11:45 -0400

    delete __init__

--- app/EventHandler/__init__.py
+++
@@ -1,9 +0,0 @@
-__all__ = [
-    "danmaku_emoticon_handler",
-    "danmaku_text_handler",
-    "enter_handler",
-    "gift_handler",
-    "guard_handler",
-    "warning_handler",
-    "sc_handler",
-]
