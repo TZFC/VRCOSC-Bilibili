5c30a9ebc052cbe22cdc06d1b4b42231becafe72
天子方辰 <chenfangchen2000@gmail.com>
2025-08-27 14:51:43 -0400

    Redirect script errors to error-log

--- RUNME.bat
+++ RUNME.bat
@@ -3,7 +3,7 @@ setlocal
 cd /d "%~dp0"
 
 call ".venv\Scripts\activate.bat"
-python -u ".\main.py" --log info
+python -u ".\main.py" --log info 2>>error-log.txt
 
 echo Python exited with %errorlevel%
 pause
